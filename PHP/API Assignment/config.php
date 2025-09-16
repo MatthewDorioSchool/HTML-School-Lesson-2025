09865e40a02c0ef67807903fbcd7a8cb36d87b98
MatthewDorioSchool <200618886@student.georgianc.on.ca>
2025-09-16 00:00:32 -0400

    Add files via upload

---
+++ PHP/API Assignment/config.php
@@ -0,0 +1,6 @@
+<?php
+// Configureation file
+
+define("DOG_API_KEY", "live_nZCzvZnQY0S1ejkPeDWSWEY1kB3euSDkI1VTNOG8qbvx9AugBn47cGI3tlnT0S6n");
+
+define("DOG_BASE_URL", "https://api.thedogapi.com/v1");
